71faf4910a1536db7d39db738c7097511511b164
RohanGada <rohangadaconsultant@localhost>
2017-11-17 15:32:01 +0530

    3d carousel

--- www/js/app.js
+++ www/js/app.js
@@ -7,113 +7,113 @@
 // var _ = require('lodash');
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
-
-$ionicConfigProvider
-  .backButton
-  .previousTitleText(false)
-  .text('');
-// $ionicConfigProvider
-//   .views
-//   .maxCache(0);
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html',
-        controller:'SearchCtrl'
-      }
-    }
-  })
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.managerapproval', {
-      url: '/managerapproval',
-      views: {
-        'menuContent': {
-templateUrl : 'templates/manager-approval.html',
-controller : 'ManagerApprovalCtrl'
-        }
-      }
-    })
-    .state('app.employee', {
-      url: '/employee',
-      views: {
-        'menuContent': {
-templateUrl : 'templates/employee.html',
-controller : 'EmployeeCtrl'
-        }
-      }
-    })
-    .state('app.travelapproval', {
-      url: '/travelapproval',
-      views: {
-        'menuContent': {
-templateUrl : 'templates/travel-approval.html',
-controller : 'TravelApprovalCtrl'
-        }
-      }
-    })
-    .state('app.mypayslip', {
-      url: '/mypayslip',
-      views: {
-        'menuContent': {
-templateUrl : 'templates/mypayslip.html',
-controller : 'MyPaySlipCtrl'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    $ionicConfigProvider
+      .backButton
+      .previousTitleText(false)
+      .text('');
+    // $ionicConfigProvider
+    //   .views
+    //   .maxCache(0);
+    $stateProvider
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html',
+            controller: 'SearchCtrl'
+          }
+        }
+      })
+
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      .state('app.managerapproval', {
+        url: '/managerapproval',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/manager-approval.html',
+            controller: 'ManagerApprovalCtrl'
+          }
+        }
+      })
+      .state('app.employee', {
+        url: '/employee',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/employee.html',
+            controller: 'EmployeeCtrl'
+          }
+        }
+      })
+      .state('app.travelapproval', {
+        url: '/travelapproval',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/travel-approval.html',
+            controller: 'TravelApprovalCtrl'
+          }
+        }
+      })
+      .state('app.mypayslip', {
+        url: '/mypayslip',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/mypayslip.html',
+            controller: 'MyPaySlipCtrl'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/search');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/search');
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -87,7 +87,7 @@ angular.module('starter.controllers', [])
       })
       .then(function (modal) {
         console.log(modal)
-        $scope.pdfModal = modal;
+$scope.pdfModalbreak = modal;
         $scope.googleDocViewer = $sce.trustAsResourceUrl("http://docs.google.com/gview?url=" + PDFUrl + "&embedded=true");
 
       });
@@ -230,8 +230,7 @@ angular.module('starter.controllers', [])
       window
         .CP[name]
         .previousSwipe();
-    }
-    console.log('search')
+    };
     $scope.canDrag = true
     $scope.dragged = function (ev, name) {
       //       if($scope.canDrag){
@@ -246,6 +245,60 @@ angular.module('starter.controllers', [])
       //     }
 
     }
+
+$(function () {
+  var showcase = $("#showcase")
+
+  showcase.Cloud9Carousel({
+    yPos: 42,
+    yRadius: 48,
+    mirrorOptions: {
+      gap: 12,
+      height: 0.2
+    },
+    buttonLeft: $(".nav > .left"),
+    buttonRight: $(".nav > .right"),
+    autoPlay: true,
+    bringToFront: true,
+    onRendered: showcaseUpdated,
+    onLoaded: function () {
+      showcase.css('visibility', 'visible')
+      showcase.css('display', 'none')
+      showcase.fadeIn(1500)
+    }
+  })
+
+  function showcaseUpdated(showcase) {
+    var title = $('#item-title').html($(showcase.nearestItem()).attr('alt'))
+    var c = Math.cos((showcase.floatIndex() % 1) * 2 * Math.PI)
+    title.css('opacity', 0.5 + (0.5 * c))
+  }
+
+  // Simulate physical button click effect
+  $('.nav > button')
+    .click(function (e) {
+      var b = $(e.target).addClass('down')
+      setTimeout(function () {
+        b.removeClass('down')
+      }, 80)
+    })
+
+  $(document).keydown(function (e) {
+    //
+    // More codes: http://www.javascripter.net/faq/keycodes.htm
+    //
+    switch (e.keyCode) {
+        /* left arrow */
+      case 37:
+        $('.nav > .left').click()
+        break
+
+        /* right arrow */
+      case 39:
+        $('.nav > .right').click()
+    }
+  })
+});
     // $(document)
     // .ready(function () {
 
